c9ac9fbcee61fe8a701616e33073bbe91aaab08d
Simon Bjerre <simon.bjerre@team.blue>
2024-07-13 07:30:52 +0200

    w

---
+++ src/utils/metarDecode.jsx
@@ -0,0 +1,8 @@
+import metarParser from 'aewx-metar-parser';
+
+function metarDecode(icao) {
+    const response = fetch("https://metar.vatsim.net/"+icao)
+    console.log(response)
+}
+
+export default metarDecode
