0330facf018c517980b836d636e7d32b841e51b8
Simon Bjerre <bjerrecs@protonmail.com>
2024-07-26 23:16:18 +0200

    nuke vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://cphlive.vatsim-scandinavia.org/',
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8,
-  }),
 });
